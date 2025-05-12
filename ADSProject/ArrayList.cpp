15f59bff57df555e3249806d23a53eef10c7e842
work204 <titanistikman@gmail.com>
2025-05-12 18:54:30 +0300

    Add ArrayList

---
+++ ADSProject/ArrayList.cpp
@@ -0,0 +1,46 @@
+#include "ArrayList.h"
+
+void ArrayList::add(int value, int size) {
+	if (size < 0) {
+		return;
+	}
+	if (array == nullptr) {
+		array = new int[1];
+		array[0] = value;
+	}
+
+	size++;
+	int* copy = new int[size];
+
+	for (int i = 0; i < size-1; i++)
+	{
+		copy[i] = array[i];
+	}
+
+	copy[size + 1] = value;
+	size++;
+
+	array = copy;
+}
+
+
+
+void ArrayList::add(int index, int value, int size) {
+	if (array == nullptr) {
+		array = new int[index + 1];
+		array[index];
+	}
+	
+	if (index < 0 || size < 0) {
+		return;
+	}
+
+	int* copy = new int[size + 1];
+	
+	for (int i = 0, j = index; i < size + 1; i++)
+	{
+
+	}
+
+	
+}
---
+++ ADSProject/ArrayList.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class ArrayList
+{
+private:
+	int* array;
+	int size;
+public:
+	ArrayList();
+	~ArrayList();
+
+	void add(int value, int size);
+	void add(int index, int value, int size);
+	void addAll(int* values, int size);
+	void addAll(int index, int* values, int size);
+
+	void remove(int index);
+	void remove();
+
+	int get(int index);
+
+	int size();
+	bool isEmpty() { return size() == 0; };
+	void set(int index, int value);
+	void clear();
+	string toString();
+};
+
